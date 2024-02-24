bcebee5f38d2492b39cb9b896e92f2b2c60ccc6b
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2024-02-24 22:19:27 +0000

    Update import pygame.py

--- import pygame.py
+++ import pygame.py
@@ -13,7 +13,8 @@ timer = pygame.time.Clock()
 
 #game vars
 wall_thickness =10
-
+gravity = 0.5
+bounce_stop = 0.3
 
 
 def draw_walls():
@@ -25,12 +26,46 @@ def draw_walls():
         return wall_list
 
 
+class Ball:
+        def __init__(self, position, radius, color, mass, retention, velocity, id):
+                self.position = position
+                self.radius = radius
+                self.color = color
+                self.mass = mass
+                self.retention = retention
+                self.velocity = velocity
+                self.id = id
+                
+                self.circle = ''
+                
+        def draw(self):
+                self.circle = pygame.draw.circle(screen, self.color, self.position, self.radius)
+                
+        
+        
+        def check_gravity(self):
+                
+                if self.position[1] < (HEIGHT - self.radius - wall_thickness/2):
+                        self.velocity[1] += gravity
+                
+                else:
+                        if self.velocity[1] > bounce_stop:
+                                self.velocity[1] = -self.velocity[1]*self.retention
+
+
+ball1 = Ball([50, 50], 30, 'blue', 100, 0.9, [1, 1], 1)
+ball2 = Ball([200, 200], 30, 'blue', 100, 0.9, [1, 1], 1)
+
+
 #main game loop
 run = True
 
 while run:
         timer.tick(fps)
         screen.fill('black')
+        
+        ball1.draw()
+        ball2.draw()
         
         walls = draw_walls()
         
